a3e1d90e7dfbb902a8f3eb198f9bade86f3e1948
Tony de Faria <tonydefaria@iigethr.com>
2020-06-06 15:14:56 +0100

    trying to sort forms problem

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,8 @@ export default function Home() {
             </p>
           </a>
         </div>
-        <form name="contact" method="POST" data-netlify="true">
+        <form name='contact' method='POST' enctype='application/x-www-form-urlencoded' data-netlify="true">
+          <input type='hidden' name='form-name' value='contact' />
           <p>
             <label>Your Name: <input type="text" name="name" /></label>
           </p>
